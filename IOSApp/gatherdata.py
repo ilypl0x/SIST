ed41f8d46a2b7c2437c7e1665f31021b82f413a7
Justin <63241141+ilypl0x@users.noreply.github.com>
2020-05-15 12:28:57 -0400

    Add files via upload
    slight change to remove requests_html

--- IOSApp/gatherdata.py
+++ IOSApp/gatherdata.py
@@ -1,19 +1,37 @@
 from bs4 import BeautifulSoup
-import requests_html
+from lxml import html
 import requests
 import json
 
+# def get_ticker_pricev2(ticker):
+
+#     price_code = 'span[data-reactid="32"]'
+#     name_code = 'h1[data-reactid="7"]'
+#     close_code = 'span[data-reactid="35"]'
+
+#     url = 'https://in.finance.yahoo.com/quote/' + ticker
+#     session = requests_html.HTMLSession()
+#     r = session.get(url)
+#     soup = BeautifulSoup(r.content, 'lxml')
+#     price_location = soup.find("div", {"id":"Lead-3-QuoteHeader-Proxy"})
+
+#     close = price_location.select(close_code)[0].text
+#     name = price_location.select(name_code)[0].text
+#     curr_price = float(price_location.select(price_code)[0].text)
+
+#     return close,name,curr_price
+
+
 def get_ticker_price(ticker):
 
-    price_code = 'span[data-reactid="32"]'
+    price_code = 'span[data-reactid="14"]'
     name_code = 'h1[data-reactid="7"]'
-    close_code = 'span[data-reactid="35"]'
+    close_code = 'span[data-reactid="18"]'
 
     url = 'https://in.finance.yahoo.com/quote/' + ticker
-    session = requests_html.HTMLSession()
-    r = session.get(url)
-    soup = BeautifulSoup(r.content, 'lxml')
-    price_location = soup.find("div", {"id":"Lead-3-QuoteHeader-Proxy"})
+    html_page = requests.get(url).content
+    soup = BeautifulSoup(html_page, 'lxml')
+    price_location = soup.find("div", attrs={"id":"Col1-2-QuoteHeader-Proxy"})
 
     close = price_location.select(close_code)[0].text
     name = price_location.select(name_code)[0].text
@@ -34,3 +52,6 @@ def get_symbol(symbol):
                 return x['name']
     except:
             return None
+
+# if __name__ in "__main__":
+#     print(get_ticker_price("JPM"))
